60864412b6e34a79fd49a488722c0a04b38ddd13
Augusto Velez <augustovelez@Augustos-MacBook-Air.local>
2020-07-29 10:39:57 -0500

    modificaciones de estilos

--- dist/js/darkmode.js
+++ dist/js/darkmode.js
@@ -1,13 +1,13 @@
-function darkMode(){
+function darkMode() {
   console.log("Dark mode clicked!");
   var bod = document.getElementById("page-top");
   bod.classList.toggle("darkmode");
   if (document.getElementById('inputDarkMode').checked) {
     console.log("Checked!");
-    document.getElementById("sideNav").style.boxShadow="4px 3px 4px 4px #3333336e";
-    
-} else {
+    document.getElementById("sideNav").style.boxShadow = "2px 1px 6px 2px #3333336e";
+
+  } else {
     console.log("NOT Checked");
     // element.classList.toggle("darkmode");
-}
+  }
 }
